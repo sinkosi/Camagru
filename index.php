68b69e4afe6111fb6eaef8d2c50b280e81acd4f7
sinkosi <sinkosi@student.wethinkcode.co.za>
2019-10-21 17:14:48 +0200

    Let's get this thing going

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+print "HELLO WORLD, THis is me trying something"
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Basics</title>
+
+<body style="background-color: #FFC0CB;">
+<header>
+    <h1 style="text-align: center; color: #ffffff">-&#42-so bigger -&#42-</h1>
+
+</header>
+<table>
+    <tr>
+        <td><IMG src="https://hisense.co.za/wp-content/uploads/2018/11/Takealot.png" width="500" height="300"></td>
+        <td><img src="https://images-na.ssl-images-amazon.com/images/G/01/gc/designs/livepreview/amazon_dkblue_noto_email_v2016_us-main._CB468775337_.png" width="500" height="300"></td>
+    </tr>
+    <tr>
+        <td><A href ="https://www.takealot.com">takealot</A></td>
+        <td><A href="https://www.amazon.com">amazon</A></td>
+    </tr>
+</table>
+<hr>
+<p style="text-align: right; font-style: italic">&#169 sinkosi 2019</p>
+</body>
+</head>
+</html>
