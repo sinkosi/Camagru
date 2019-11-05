1cfe6c1d62681f45b66a4483c8abc0f80aa28335
sinkosi <sinkosi@student.wethinkcode.co.za>
2019-11-05 17:38:57 +0200

    organise this thang

--- gallery2.php
+++ gallery2.php
@@ -6,16 +6,17 @@ error_reporting(E_ALL);
 include './config/createConnection.php';
 require './config/database.php';
 
+include('header.php');
 // Get images from the database
 $query = $dbn->query("SELECT * FROM images ORDER BY uploaded_on DESC");
 
 if($query->num_rows > 0){
     while($row = $query->fetch_assoc()){
         $imageURL = 'images/'.$row["file_name"];
-include('header.php')
+
 ?>
 
-    <img src="<?php echo $imageURL; ?>" alt="" height="640" width="480"/>
+    <img src="<?php echo $imageURL; ?>" alt="" height="320" width=""/>
 <?php }
 }else{ ?>
     <p>No image(s) found...</p>
