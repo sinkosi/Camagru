2630bb899eacc984a25d60e92eb6c51b2e49018a
sinkosi <sinkosi@student.wethinkcode.co.za>
2019-11-20 12:03:01 +0200

    File management success

--- save_img.php
+++ save_img.php
@@ -14,16 +14,30 @@ $fileName = basename($_FILES["file"]["name"]);
 $targetFilePath = $targetDir . $fileName;
 $fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION);*/
 
+/*function RandomString()
+{
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $randstring = '';
+    for ($i = 0; $i < 10; $i++) {
+        $randstring = $characters[rand(0, strlen($characters))];
+    }
+    return $randstring;
+}
 
+RandomString();*/
 
+$img_name = $_SESSION["username"].date("Y-m-d H:i:s").'.png';
 $img = base64_decode($_POST["img"]);
 $img = imagecreatefromstring($img);
-imagepng($img, "images/image.png");
+imagepng($img, "images/".$img_name);
 
-$insert = $db->query("INSERT into images (source, userid, uploaded_on) VALUES ('image.png', '".$_SESSION["id"]."', NOW())");
+/*$fileName = RandomString().'.png';
+echo $fileName;*/
+
+$insert = $db->query("INSERT into images (source, userid, uploaded_on) VALUES ('".$img_name."', '".$_SESSION["id"]."', NOW())");
                 
     if($insert){
-        $statusMsg = "The file ".$fileName. " has been uploaded successfully by ".($_SESSION["username"]).".";
+        $statusMsg = "The file ".$img_name. " has been uploaded successfully by ".($_SESSION["username"]).".";
     }else{
         $statusMsg = "File upload failed, please try again.";
     }
