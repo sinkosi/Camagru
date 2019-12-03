2958cc858359856c693012152af501c5aa4eb14a
Sibonelo NKOSI <sinkosi@c4r23s1.wethinkcode.co.za>
2019-12-03 12:27:38 +0200

    Pagination and modifications

--- gallery.php
+++ gallery.php
@@ -21,13 +21,41 @@ include('header.php');
 
 <br>
 <?php
-// Get images from the database
-$query = $dbn->query("SELECT * FROM images ORDER BY uploaded_on DESC");
+//define results per page
+$results_per_page = 5;
 
+//find the number of results stored in database
+$sql = "SELECT * FROM images";
+$result = mysqli_query($db, $sql);
+$number_of_results = mysqli_num_rows($result);
+$number_of_pages = ceil($number_of_results / $results_per_page);
+
+//determine which page number visitor is currently on
+if (!isset($_GET['page'])){
+    $page = 1;
+} else {
+    $page = $_GET['page'];
+}
+
+//determine the sql Start LIMIT number
+$this_page_first_result = ($page-1)*$results_per_page;
+
+//retrieve selected results from database and display them on page
+$sql = "SELECT * FROM images ORDER BY uploaded_on DESC LIMIT " . $this_page_first_result . ',' . $results_per_page;
+$result = mysqli_query($db, $sql);
+
+if ($result > 0){
+    while ($row = mysqli_fetch_array($result)){
+        $imageURL ='images/'.$row['source'];   
+
+
+/*// Get images from the database
+$query = $dbn->query("SELECT * FROM images ORDER BY uploaded_on DESC");
+//$query = $dbn->query("SELECT * FROM images LIMIT 5");
 if($query->num_rows > 0){
     while($row = $query->fetch_assoc()){
         $imageURL = 'images/'.$row["source"];
-
+*/
 ?>
 
     <img src="<?php echo $imageURL; ?>" alt="" height="320" width=""/>
@@ -40,6 +68,10 @@ if($query->num_rows > 0){
 <?php }
 }else{ ?>
     <p>No image(s) found...</p>
-<?php } 
+<?php }
+//display the links to the page
+for ($page=1;$page<=$number_of_pages;$page++) {
+    echo '<a href="gallery.php?page=' . $page . '">' . $page . "</a>";
+}
 include('footer.php')
 ?>
